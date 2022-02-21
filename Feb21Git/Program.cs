ddfb07ed5a15065fbbb4bb2bcea59f5ac9baed1b
SindhujaObulasetti <sindhujaobulasetti17@gmail.com>
2022-02-21 10:38:52 +0530

    commit2

--- Feb21Git/Program.cs
+++ Feb21Git/Program.cs
@@ -10,6 +10,10 @@ namespace Feb21Git
     {
         static void Main(string[] args)
         {
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Welcome to Git");
+            }
         }
     }
 }
